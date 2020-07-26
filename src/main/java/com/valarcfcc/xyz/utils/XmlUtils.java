9007bf0c9225c5b9b5ce2af10c5cb532155921b1
valar cf <chengfan001@hotmail.com>
2020-07-26 19:34:13 +0800

    1

--- src/main/java/com/valarcfcc/xyz/utils/XmlUtils.java
+++ src/main/java/com/valarcfcc/xyz/utils/XmlUtils.java
@@ -2,6 +2,7 @@ package com.valarcfcc.xyz.utils;
 
 import com.sun.istack.internal.NotNull;
 import com.sun.xml.internal.bind.marshaller.NamespacePrefixMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;;
 
 import java.lang.reflect.Field;
@@ -20,6 +21,7 @@ import java.util.Map;
  * @Date: 2020/7/22 22:53
  * @Description:
  */
+@Slf4j
 public class XmlUtils {
     public static String beanToXml(Object object, Class<?> clazz) throws JAXBException {
         String result = StringUtils.EMPTY;
@@ -106,34 +108,22 @@ public class XmlUtils {
     public static String objectToXML(@NotNull Object obj, @NotNull String node) throws IllegalAccessException {
         StringBuffer soapResultData = new StringBuffer();
         Class<?> clazz = obj.getClass();
-        if (obj instanceof String || obj instanceof StringBuffer
-                || obj instanceof Boolean || obj instanceof Byte
-                || obj instanceof Integer || obj instanceof Short
-                || obj instanceof Character || obj instanceof Long
-                || obj instanceof Float || obj instanceof Double
-                || obj instanceof BigDecimal || obj instanceof BigInteger) {
-            System.out.println("--------Java包装类与math--------");
+        if (isBaseOrStringOrMath(obj)) {
             return obj.toString();
         }
         for (Field field : clazz.getDeclaredFields()) {
             field.setAccessible(true);
             String fieldName = field.getName();
             Object value = field.get(obj);
-            Class<?> valueClass = null;
             if (Objects.isNull(value)) {
-            } else {
-                valueClass = value.getClass();
-                System.out.println(valueClass.getName());
-            }
-            if (value instanceof List) {
-                if (((List) value).isEmpty()) {
-                    continue;
-                } else {
-                    for (Object o : (List) value) {
-                        soapResultData.append("<" + node + ":" + fieldName + ">");
-                        soapResultData.append(objectToXML(o, node));
-                        soapResultData.append("</" + node + ":" + fieldName + ">");
-                    }
+                soapResultData.append("<" + node + ":" + fieldName + ">");
+                soapResultData.append("</" + node + ":" + fieldName + ">");
+                continue;
+            } else if (value instanceof List) {
+                for (Object o : (List) value) {
+                    soapResultData.append("<" + node + ":" + fieldName + ">");
+                    soapResultData.append(objectToXML(o, node));
+                    soapResultData.append("</" + node + ":" + fieldName + ">");
                 }
                 continue;
             } else if (value instanceof Map) {
@@ -146,34 +136,24 @@ public class XmlUtils {
                 }
                 soapResultData.append("</" + node + ":" + fieldName + ">");
                 continue;
-            } else if (value instanceof String || value instanceof StringBuffer
-                    || value instanceof Boolean || value instanceof Byte
-                    || value instanceof Integer || value instanceof Short
-                    || value instanceof Character || value instanceof Long
-                    || value instanceof Float || value instanceof Double
-                    || value instanceof BigDecimal || value instanceof BigInteger) {
+            } else if (isBaseOrStringOrMath(value)) {
                 soapResultData.append("<" + node + ":" + fieldName + ">");
                 soapResultData.append(value.toString());
                 soapResultData.append("</" + node + ":" + fieldName + ">");
-                System.out.println("--------Java包装类与math--------");
                 continue;
-            }else if (value instanceof Object) {
+            } else if (value instanceof Object) {
                 soapResultData.append("<" + node + ":" + fieldName + ">");
                 soapResultData.append(objectToXML(value, node));
                 soapResultData.append("</" + node + ":" + fieldName + ">");
-               System.out.println("--------Object--------");
-            }
-            else if (Objects.isNull(value)) {
-                soapResultData.append("<" + node + ":" + fieldName + ">");
-                soapResultData.append("</" + node + ":" + fieldName + ">");
-                System.out.println("--------null--------");
-                continue;
-            }
-            else {
+            } else {
                 throw new IllegalAccessException("Bean类型错误！");
             }
         }
         return soapResultData.toString();
+    }
+
+    private static Boolean isBaseOrStringOrMath(Object obj) {
+        return obj instanceof String || obj instanceof StringBuffer || obj instanceof Boolean || obj instanceof Byte || obj instanceof Integer || obj instanceof Short || obj instanceof Character || obj instanceof Long || obj instanceof Float || obj instanceof Double || obj instanceof BigDecimal || obj instanceof BigInteger;
     }
 
     /**
