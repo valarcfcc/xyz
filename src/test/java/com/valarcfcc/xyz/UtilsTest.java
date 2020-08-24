137ebbb81331231a7f14d1dc2e6ccb638c2c7f34
valarcfcc <chengfan001@hotmail.com>
2020-08-24 13:27:29 +0800

    excel

--- src/test/java/com/valarcfcc/xyz/UtilsTest.java
+++ src/test/java/com/valarcfcc/xyz/UtilsTest.java
@@ -1,13 +1,18 @@
 package com.valarcfcc.xyz;
 
+import com.valarcfcc.xyz.utils.ExcelUtils;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.List;
 
 @SpringBootTest
 public class UtilsTest {
     @Test
     public void excelTest(){
-        String filePath = "C:\\Users\\cheng\\Desktop\\test.xlsx";
-
+        String filePath = "D://test.xlsx";
+        List<HashMap<Integer,String>> list  = ExcelUtils.excel2MapList(filePath,5,5);
+        list.forEach(System.out::println);
     }
 }
