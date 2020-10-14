611d420db621b1280f979bdaefa16d07df69f334
valarcfcc <chengfan001@hotmail.com>
2020-10-14 10:09:08 +0800

    增加printf

---
+++ src/main/java/com/valarcfcc/xyz/utils/Common.java
@@ -0,0 +1,7 @@
+package com.valarcfcc.xyz.utils;
+
+public class Common {
+    public static void println(String msg) {
+        System.out.println(msg);
+    }
+}
--- src/test/java/com/valarcfcc/xyz/setTest.java
+++ src/test/java/com/valarcfcc/xyz/setTest.java
@@ -1,11 +1,16 @@
 package com.valarcfcc.xyz;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.HashSet;
-import java.util.Set;
-
+import static com.valarcfcc.xyz.utils.Common.println;
 @SpringBootTest
+@Slf4j
 public class setTest {
+    @Test
+    public void outTest(){
+        Integer as = 1;
+        println("s{a}");
+        log.info("s{}","b");
+    }
 }
